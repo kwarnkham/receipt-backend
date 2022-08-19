17bafb1dcde5ddb3918988f9f20b1144e8010d75
Moon <kwarnkham.moon@gmail.com>
2022-08-19 21:52:43 +0630

    test_only_admin_can_save_picture

--- tests/Feature/PictureTest.php
+++ tests/Feature/PictureTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Enums\ResponseStatus;
 use App\Models\Picture;
 use App\Models\Role;
 use App\Models\User;
@@ -27,15 +28,22 @@ class PictureTest extends TestCase
         $this->user2 = User::factory()->create();
         $this->admin = User::factory()->has(Role::factory(['name' => 'admin']))->create();
     }
+
     public function test_save_picture()
     {
         $response = $this->actingAs($this->admin)->postJson('/api/picture', ['user_id' => $this->user->id, 'picture' => UploadedFile::fake()->image('test.jpg'), 'type' => 1]);
 
-        $response->assertStatus(201);
+        $response->assertCreated();
         $this->assertDatabaseCount('pictures', 1);
         $picture = Picture::find($response->json()['id']);
         $response = Http::get($picture->url());
         $this->assertTrue($response->ok());
         $this->assertTrue(Storage::disk('s3')->delete($picture->name));
+    }
+
+    public function test_only_admin_can_save_picture()
+    {
+        $response = $this->actingAs($this->user)->postJson('/api/picture', ['user_id' => $this->user->id, 'picture' => UploadedFile::fake()->image('test.jpg'), 'type' => 1]);
+        $response->assertStatus(ResponseStatus::UNAUTHORIZED->value);
     }
 }

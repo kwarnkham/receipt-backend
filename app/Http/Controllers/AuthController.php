10ba402c6186cf364f1c9620ae195acd84fd3c80
SAI KWARN KHAM <kwarnkham.moon@gmail.com>
2022-09-08 16:20:05 +0630

    rework on cors

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,7 +12,7 @@ class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        Log::info($request->header('host'));
+        Log::info($request->headers->all());
         $data = $request->validate([
             'mobile' => ['required', 'exists:users,mobile'],
             'password' => ['required']
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function (Request $request) {
-    ddd($request);
     return view('welcome');
 });
